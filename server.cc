b566e461d2cadb57e9992d56488485fd2b53b56e
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-02-03 19:47:50 +0000

    Working.
    TODO:
    Stream to send file chunks.

--- server.cc
+++ server.cc
@@ -5,42 +5,54 @@
 
 #include "app.grpc.pb.h"
 
+using app::DataReply;
+using app::DataRequest;
+using app::DataService;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using app::DataReply;
-using app::DataRequest;
-using app::DataService;
 
 class DataServiceImpl final : public DataService::Service
 {
-    Status GetData(ServerContext* context, const DataRequest* request,
-                   DataReply* reply) override
-    {
-        (void)(context);
-        (void)(request);
-        reply->set_number(42);
-        reply->set_label("Some data");
-        return Status::OK;
-    }
+private:
+  Status GetData(ServerContext* context, const DataRequest* request,
+                 DataReply* reply) override
+  {
+    (void)(context);
+    reply->set_number(m_requestCount);
+    reply->set_label(request->name());
+    ++m_requestCount;
+    return Status::OK;
+  }
+
+private:
+  std::size_t m_requestCount = 0;
 };
 
-void RunServer()
+void RunServer(const std::string& hostname, const std::string& port)
 {
-    std::string server_address("0.0.0.0:50051");
-    DataServiceImpl service;
-
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on " << server_address << std::endl;
-    server->Wait();
+  std::string server_address(hostname + ":" + port);
+  DataServiceImpl service;
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
 }
 
-int main()
+int main(int argc, char* args[])
 {
-    RunServer();
-    return 0;
+  if (argc < 3)
+  {
+    std::cout << "Usage: ./server <hostname> <port>" << std::endl;
+    std::cout << "EXAMPLE" << std::endl;
+    std::cout << "    ./server localhost 50051" << std::endl;
+    return 1;
+  }
+  const std::string hostname(args[1]);
+  const std::string port(args[2]);
+  RunServer(hostname, port);
+  return 0;
 }

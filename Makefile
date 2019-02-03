cd3b774e00f199d61d23b15d826463b5f5800c95
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-02-03 18:00:24 +0000

    Server runs.

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
-all: services messages
+all: server
 
-messages: client_server.proto
+# Messages
+client_server.pb.o: client_server.proto
 	protoc --cpp_out=. client_server.proto && \
 	g++ -std=c++11 `pkg-config --cflags protobuf grpc`  -c -o client_server.pb.o client_server.pb.cc
 
-services: client_server.proto
+# Services
+client_server.grpc.pb.o: client_server.pb.o
 	protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin`  client_server.proto && \
 	g++ -std=c++11 `pkg-config --cflags protobuf grpc`  -c -o client_server.grpc.pb.o client_server.grpc.pb.cc
 
-server: messages services
+
+server: server.cc client_server.grpc.pb.o
 	g++ -std=c++11 `pkg-config --cflags protobuf grpc` -c -o server.o server.cc && \
 	g++ client_server.pb.o client_server.grpc.pb.o server.o -L/usr/local/lib `pkg-config --libs protobuf grpc++ grpc` -o server
 
@@ -19,3 +22,5 @@ clean:
 	rm client_server.grpc.pb.cc
 	rm client_server.pb.h
 	rm client_server.pb.cc
+	rm *.o
+	rm server
--- server.cc
+++ server.cc
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <memory>
 
+#include <grpcpp/grpcpp.h>
+
+#include "client_server.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using clientserver::DataReply;
+using clientserver::DataRequest;
+using clientserver::DataService;
+
+class DataServiceImpl final : public DataService::Service
+{
+    Status GetData(ServerContext* context, const DataRequest* request,
+                   DataReply* reply) override
+    {
+        (void)(context);
+        (void)(request);
+        reply->set_number(42);
+        reply->set_label("Some data");
+        return Status::OK;
+    }
+};
+
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    DataServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main()
+{
+    RunServer();
+    return 0;
+}

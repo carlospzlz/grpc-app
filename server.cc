4fb29c42f61ddec60a3fc065310ea2bacdcb78f2
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-02-16 21:34:53 +0000

    Number and string seems working fine.
    TODO:
    File by chunks.

--- server.cc
+++ server.cc
@@ -5,29 +5,62 @@
 
 #include "app.grpc.pb.h"
 
-using app::DataReply;
-using app::DataRequest;
 using app::DataService;
+using app::FileReply;
+using app::FileRequest;
+using app::NumberReply;
+using app::NumberRequest;
+using app::StringReply;
+using app::StringRequest;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
+using grpc::StatusCode;
 
 class DataServiceImpl final : public DataService::Service
 {
+public:
+  DataServiceImpl()
+  {
+    m_numbers = {{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}};
+    m_strings = {"foo", "bar", "spam", "ham", "eggs"};
+  }
+
 private:
-  Status GetData(ServerContext* context, const DataRequest* request,
-                 DataReply* reply) override
+  Status GetNumber(ServerContext* context, const NumberRequest* request,
+                   NumberReply* reply) override
   {
     (void)(context);
-    reply->set_number(m_requestCount);
-    reply->set_label(request->name());
-    ++m_requestCount;
+    const std::string name = request->name();
+    const auto iter = m_numbers.find(name);
+    if (iter == m_numbers.cend())
+    {
+      const std::string details("Number with name " + name + " not found");
+      return Status(StatusCode::NOT_FOUND, details);
+    }
+    reply->set_number(iter->second);
+    return Status::OK;
+  }
+
+  Status GetString(ServerContext* context, const StringRequest* request,
+                   StringReply* reply) override
+  {
+    (void)(context);
+    const int index = request->index();
+    if (index < 0 || index > (m_strings.size() - 1))
+    {
+      const std::string details("String with index " + std::to_string(index) +
+                                " not found");
+      return Status(StatusCode::NOT_FOUND, details);
+    }
+    reply->set_string_(m_strings[index]);
     return Status::OK;
   }
 
 private:
-  std::size_t m_requestCount = 0;
+  std::map<std::string, int> m_numbers;
+  std::vector<std::string> m_strings;
 };
 
 void RunServer(const std::string& hostname, const std::string& port)

52973ccb15699a37b19a6c4190676391a4e275e4
Carlos Perez-Lopez <carlospzlz@gmail.com>
2019-02-18 00:28:18 +0000

    Fixed some minor problems with files

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,11 @@
+def FlagsForFile(filename, **kwargs):
+    return {
+        'flags': [
+            '-Wall',
+            '-Wextra',
+            '-Werror',
+            '-std=c++11',
+            '-x', 'c++',
+            '-isystem', '/usr/include/c++/8.2.1',
+            '-I', '/usr/include'],
+    }
